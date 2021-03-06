a92d316842f1df5434b6e44f4236379b95a39c4a
Robert <parralrob@gmail.com>
2021-03-05 23:58:35 -0400

    cambios rob

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: true,
-<<<<<<< HEAD
   firebase: {
     apiKey: "AIzaSyDaw5IGOlqumggfTTrnYDCDtCYatRRMf7M",
     authDomain: "microproyecto2-93bc3.firebaseapp.com",
@@ -10,14 +9,3 @@ export const environment = {
     messagingSenderId: "852040314916"
   }
 };
-=======
-  firebaseConfig = {
-    apiKey: "AIzaSyDaw5IGOlqumggfTTrnYDCDtCYatRRMf7M",
-    authDomain: "microproyecto2-93bc3.firebaseapp.com",
-    projectId: "microproyecto2-93bc3",
-    storageBucket: "microproyecto2-93bc3.appspot.com",
-    messagingSenderId: "852040314916",
-    appId: "1:852040314916:web:dac256748a8dede65ba8b1",
-    measurementId: "G-3N08MEGP55"
-  };
->>>>>>> 6f3dbb51e8ffd3b6a85720e62161d68704737c02
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,6 @@
 
 export const environment = {
   production: false,
-<<<<<<< HEAD
   firebase: {
     apiKey: "AIzaSyDaw5IGOlqumggfTTrnYDCDtCYatRRMf7M",
     authDomain: "microproyecto2-93bc3.firebaseapp.com",
@@ -14,17 +13,6 @@ export const environment = {
     messagingSenderId: "852040314916"
   }
 };
-=======
-  firebaseConfig = {
-    apiKey: "AIzaSyDaw5IGOlqumggfTTrnYDCDtCYatRRMf7M",
-    authDomain: "microproyecto2-93bc3.firebaseapp.com",
-    projectId: "microproyecto2-93bc3",
-    storageBucket: "microproyecto2-93bc3.appspot.com",
-    messagingSenderId: "852040314916",
-    appId: "1:852040314916:web:dac256748a8dede65ba8b1",
-    measurementId: "G-3N08MEGP55"
-  };
->>>>>>> 6f3dbb51e8ffd3b6a85720e62161d68704737c02
 
 /*
  * For easier debugging in development mode, you can import the following file
