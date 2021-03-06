04ed33049ae03852ae4fb2c42d56c72755e824da
Your Name <you@example.com>
2021-03-05 22:26:46 -0400

    cambio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -11,6 +13,33 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { ContactComponent } from './contact/contact.component';
 
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
+import { FlashMessagesModule } from 'angular2-flash-messages';
+import { MovieService } from './movies/service/movie.service';
+import { HttpModule, JsonpModule } from '@angular/http';
+
+const appRoutes: Routes = [
+
+  {path: '', component: MovieListComponent, canActivate: [AuthGuardService]},
+  {path: 'movie/:id', component: MovieDetailComponent, canActivate: [AuthGuardService]},
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'contact', component: ContactComponent},
+  
+]
+
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { environment } from '../environments/environment';
+import { AuthService } from './auth/service/auth.service';
+import { AuthGuardService } from './auth-guard/service/auth-guard.service';
+import { SamplesComponent } from './samples/samples.component';
+
+
+export const firebaseConfig = environment.firebase;
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -21,12 +50,25 @@ import { ContactComponent } from './contact/contact.component';
     LoginComponent,
     RegisterComponent,
     ContactComponent,
+    SamplesComponent,
   ],
   imports: [
     BrowserModule,
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    JsonpModule,
+    FlashMessagesModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule, 
+    AngularFireAuthModule,
+    RouterModule.forRoot(appRoutes),
+    BsDatepickerModule.forRoot(),
     AppRoutingModule
   ],
-  providers: [],
+  providers: [MovieService,
+    AuthService,
+    AuthGuardService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
