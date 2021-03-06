89f429b959bfd6f1751eb9e47a9c6ded78107e31
Your Name <you@example.com>
2021-03-06 00:52:00 -0400

    cambio

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,9 +3,10 @@ export const environment = {
   firebase: {
     apiKey: "AIzaSyDaw5IGOlqumggfTTrnYDCDtCYatRRMf7M",
     authDomain: "microproyecto2-93bc3.firebaseapp.com",
-    databaseURL: "REPLACE_WITH_DATABASE_URL",
     projectId: "microproyecto2-93bc3",
     storageBucket: "microproyecto2-93bc3.appspot.com",
-    messagingSenderId: "852040314916"
-  }
+    messagingSenderId: "852040314916",
+    appId: "1:852040314916:web:dac256748a8dede65ba8b1",
+    measurementId: "G-3N08MEGP55"
+    }
 };
