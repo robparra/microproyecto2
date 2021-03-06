1232332cbed9e158245e42889e2631d62a0e14e2
Robert <parralrob@gmail.com>
2021-03-05 22:25:37 -0400

    cambios robert

--- src/app/movies/service/movie.service.ts
+++ src/app/movies/service/movie.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { AngularFireList } from 'angularfire2/database';
 import { Observable } from 'rxjs';
 import { AngularFireAuth } from 'angularfire2/auth';
-import { Jsonp, Http } from '@angular/http';
+// import { Jsonp, Http } from '@angular/http';
 
 import 'rxjs/Rx';
 
@@ -24,8 +24,8 @@ export class MovieService {
   constructor(
     private db: AngularFirestore,
     private afAuth: AngularFireAuth,
-    private _jsonp: Jsonp,
-    private http: Http
+    // private _jsonp: Jsonp,
+    // private http: Http
 
 
   ) {
@@ -37,36 +37,36 @@ export class MovieService {
 
   // Accessing Data From TMDB API KEY
   getPopular() {
-    return this._jsonp.get('https://api.themoviedb.org/3/discover/movie?callback=JSONP_CALLBACK&sort_by=popularity.desc&api_key=' + this.apiKey)
-      .map(res => res.json());
+    // return this._jsonp.get('https://api.themoviedb.org/3/discover/movie?callback=JSONP_CALLBACK&sort_by=popularity.desc&api_key=' + this.apiKey)
+    //   .map(res => res.json());
   }
 
   getInTheaters() {
-    return this._jsonp.get('https://api.themoviedb.org/3/discover/movie?callback=JSONP_CALLBACK&primary_release_date.gte=2016-09-15&primary_release_date.lte=2017-10-04&api_key=' + this.apiKey)
-      .map(res => res.json());
+    // return this._jsonp.get('https://api.themoviedb.org/3/discover/movie?callback=JSONP_CALLBACK&primary_release_date.gte=2016-09-15&primary_release_date.lte=2017-10-04&api_key=' + this.apiKey)
+    //   .map(res => res.json());
   }
 
   searchMovies(keyword: string) {
-    return this._jsonp.get('https://api.themoviedb.org/3/search/movie?callback=JSONP_CALLBACK&query=' + 
-    keyword + '&sort_by=popularity.desc&api_key=' + this.apiKey)
-      .map(res => res.json());
+    // return this._jsonp.get('https://api.themoviedb.org/3/search/movie?callback=JSONP_CALLBACK&query=' + 
+    // keyword + '&sort_by=popularity.desc&api_key=' + this.apiKey)
+    //   .map(res => res.json());
   }
 
   searchEntries(keywords: Observable<string>) {
-    return keywords.debounceTime(400)
-                    .distinctUntilChanged()
-                      .switchMap(keyword => this.searchMovies(keyword));
+    // return keywords.debounceTime(400)
+    //                 .distinctUntilChanged()
+    //                   .switchMap(keyword => this.searchMovies(keyword));
   }
 
   getMovie(id: string) {
-    return this._jsonp.get('https://api.themoviedb.org/3/movie/' + id + '?callback=JSONP_CALLBACK&api_key=' + this.apiKey)
-      .map(res => res.json());
+    // return this._jsonp.get('https://api.themoviedb.org/3/movie/' + id + '?callback=JSONP_CALLBACK&api_key=' + this.apiKey)
+    //   .map(res => res.json());
   }
 
 
   getMoviesByDate(start: string, end: string) {
-    return this._jsonp.get('https://api.themoviedb.org/3/discover/movie?callback=JSONP_CALLBACK&primary_release_date.gte=' + start + '&primary_release_date.lte=' + end + '&api_key=' + this.apiKey)
-      .map(res => res.json());
+    // return this._jsonp.get('https://api.themoviedb.org/3/discover/movie?callback=JSONP_CALLBACK&primary_release_date.gte=' + start + '&primary_release_date.lte=' + end + '&api_key=' + this.apiKey)
+    //   .map(res => res.json());
   }
 
 
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { AuthService } from '../auth/service/auth.service';
+// import { AuthService } from '../auth/service/auth.service';
 import { Router } from '@angular/router';
-import { FlashMessagesService } from 'angular2-flash-messages';
+// import { FlashMessagesService } from 'angular2-flash-messages';
 
 @Component({
   selector: 'app-register',
@@ -11,24 +11,24 @@ import { FlashMessagesService } from 'angular2-flash-messages';
 export class RegisterComponent implements OnInit {
 
   constructor(
-    private authService: AuthService,
+    // private authService: AuthService,
     private router: Router,
-    private flashMessagesService: FlashMessagesService
+    // private flashMessagesService: FlashMessagesService
   ) { }
 
   ngOnInit(): void {
   }
 
   onSubmit(){
-    this.authService.register(this.email, this.password)
-      .then((res) => {
-        this.flashMessagesService.show('New user registered', {cssClass: 'alert-success', timeout: 4000});
-        this.router.navigate(['/']);
-      })
-      .catch((err) => {
-        this.flashMessagesService.show(err.message, {cssClass: 'alert-danger', timeout: 4000});
-        this.router.navigate(['/register']);
-      });
+    // this.authService.register(this.email, this.password)
+    //   .then((res) => {
+    //     this.flashMessagesService.show('New user registered', {cssClass: 'alert-success', timeout: 4000});
+    //     this.router.navigate(['/']);
+    //   })
+      // .catch((err) => {
+      //   this.flashMessagesService.show(err.message, {cssClass: 'alert-danger', timeout: 4000});
+      //   this.router.navigate(['/register']);
+      // });
   }
 
 }
